d4faf19f8eca5ea624ed304d957e6c4f79967085
Badri <programmingwithbadri@outlook.com>
2020-07-13 15:35:50 +0530

    Store all the events

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+// Event data store
+const events = [];
+
 // Will be called by other microservices when new event occurs
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  // Push the event to data store
+  events.push(event);
 
   // Pass the event to posts service
   axios.post('http://localhost:4000/events', event);
@@ -22,6 +28,12 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+// If any service is down for some time,
+// then can request here to get all the events from the event data store
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
